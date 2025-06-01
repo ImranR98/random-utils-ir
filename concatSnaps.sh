c4b1c5cd13ced0d2522ddb91dcfc85198f154db3
Imran Remtulla <contact@imranr.dev>
2025-05-31 21:00:23 -0400

    Improve concatSnaps.sh

--- concatSnaps.sh
+++ concatSnaps.sh
@@ -15,7 +15,7 @@ cd "$TARGET_DIR"
 CONCAT_LIST="$TEMP_DIR/concat_list.txt"
 > "$CONCAT_LIST"
 
-FILES=$(ls -t Snapchat-*.mp4)
+FILES=$(ls -tr Snapchat-*.mp4)
 for file in $FILES; do
     BASENAME=$(basename "$file" .mp4)
     REENCODED_FILE="$TEMP_DIR/${BASENAME}_reencoded.mp4"
@@ -23,7 +23,7 @@ for file in $FILES; do
     echo "file '$REENCODED_FILE'" >> "$CONCAT_LIST"
 done
 
-OUTPUT_FN=Snap_"$(date -d @"$(stat --format='%Y' "$(echo "$FILES" | tail -1)")" --utc +%Y-%m-%d-%H%M%S)".mp4
+OUTPUT_FN=Snap_"$(date -d @"$(stat --format='%Y' "$(echo "$FILES" | head -1)")" --utc +%Y-%m-%d-%H%M%S)".mp4
 ffmpeg -f concat -safe 0 -i "$CONCAT_LIST" -c copy "$TARGET_DIR"/"$OUTPUT_FN"
 
 echo "$TARGET_DIR"/"$OUTPUT_FN"
